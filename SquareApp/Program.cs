f0fa0673a303999b30fce71581c42213faab363f
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-05-02 14:01:44 +0400

    Пушь

--- SquareApp/Program.cs
+++ SquareApp/Program.cs
@@ -1,22 +1,35 @@
 ﻿
 // track 
 using SquareApp;
-Square square = new Square(5);
+
+Random random = new Random(); 
+
+int  x00 = random.Next(-10,10);
+int  y00 = random.Next(-10, 10);
+int  a = random.Next(4, 8);
+
+Square square = new Square(a, new Point(x00, y00));
 
 
-Console.WriteLine( "___Создадим квардрат в  точке 0_0 размером  в  5 __info____");
+Console.WriteLine( $"___Создадим квадрат в  точке [{x00}.{y00}] размером  в  {a} __info____");
 Console.WriteLine(square.Info());
 
 
-Console.WriteLine("_____Повернем на  90 градусов____");
-square.Spin(90);
+int moveX   =random.Next(-3, 3);
+int moveY =random.Next(-3, 3);
+
+
+Console.WriteLine($"___ переместим  вдоль x на {moveX}"); ;
+Console.WriteLine($"___ переместим  вдоль y на {moveY}"); ;
 Console.WriteLine(square.Info());
 
 
-Console.WriteLine("_____Повернем на  - 90 градусов____");
-square.Spin(- 90);
+double spin = random.Next(-90 , 90);
+
+Console.WriteLine($"_____Повернем на  {spin} градусов____");
+square.Spin(spin);
+
+Console.WriteLine("___Итоговый  результат ");
 Console.WriteLine(square.Info());
 
-Console.WriteLine("_____Повернем на  45 градусов____");
-square.Spin(45);
-Console.WriteLine(square.Info());
+
